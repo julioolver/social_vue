a547665ef759e9ca9f61cfa19d5832be7808b0f5
Julio Cesar <juliocesar.olver@gmail.com>
2020-09-01 17:09:24 -0300

    first commit

---
+++ src/components/social/CardConteudoVue.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="card">
+  <div class="card-content">
+    <span class="card-title">Card Title</span>
+    <div class="row valign-wrapper">
+        <grid-vue tamanho="2">
+          <img src="https://materializecss.com/images/yuna.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+        </grid-vue>
+        <grid-vue tamanho="10">
+          <span class="black-text">
+            <strong>Maria Silva</strong> - <small>09/07/202 17:45</small>
+          </span>
+        </grid-vue>
+      </div>
+  </div>
+</div>
+</template>
+
+<script>
+import GridVue from '@/components/layouts/GridVue'
+
+export default {
+  name: 'CardConteudoVue',
+  props: [
+      
+  ],
+  data () {
+    return {
+     
+    }
+  },
+  components: {
+    GridVue
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <card-conteudo-vue>
+  </card-conteudo-vue>
+</template>
+
+<script>
+import CardConteudoVue from '@/components/social/CardConteudoVue'
+export default {
+  name: 'Home',
+  data () {
+    return {
+      
+    }
+  },
+  components: {
+    CardConteudoVue,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
